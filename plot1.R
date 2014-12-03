865750eb4ca1dad0a64d08d303facf2ea23da4e5
davekirby <dave.x.kirby@gmail.com>
2014-12-03 21:26:33 +0000

    got plot2 working

--- plot1.R
+++ plot1.R
@@ -1,20 +1,15 @@
 # Create the first plot for the course project
 
-# read the data file
-datafile  <- "./household_power_consumption.txt"
-data = read.csv2(datafile, na.strings="?", dec=".")
+# Read the data file - pipe it through grep to get just the rows we want
+# also need to get the first line for the headers
+datafile  <- pipe("egrep '^Date|^[12]/2/2007' ./household_power_consumption.txt")
+data = read.csv2(datafile, na.strings="?", dec=".", as.is=c("Date", "Time"))
 
 # strip NA entries
-data2 <- data2[!is.na(data2$Global_active_power),]
-
-# Convert Date column to date objects
-data2$Date <- as.Date(data2$Date, "%d/%m/%Y")
-
-# extract a subset for the dates we are interested in
-data3 <- data2[data2$Date >= as.Date("2007-02-01") & data2$Date <= as.Date("2007-02-02"),]
+data2 <- data[!is.na(data$Global_active_power),]
 
 # create the plot
 png("plot1.png", width=480, height=480)
-hist(data3$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+hist(data2$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
 # Done.  Go to bed
--- plot2.R
+++ plot2.R
@@ -1,26 +1,19 @@
 # Create the second plot for the course project
 
-# read the data file
-datafile  <- "./household_power_consumption.txt"
-data = read.csv2(datafile, na.strings="?", dec=".")
+# Read the data file - pipe it through grep to get just the rows we want
+# also need to get the first line for the headers
+datafile  <- pipe("egrep '^Date|^[12]/2/2007' ./household_power_consumption.txt")
+data = read.csv2(datafile, na.strings="?", dec=".", as.is=c("Date", "Time"))
 
 # strip NA entries
 data2 <- data[!is.na(data$Global_active_power),]
 
-# Convert Date column to date objects
-data2$Date <- as.Date(data2$Date, "%d/%m/%Y")
-
-# extract a subset for the dates we are interested in
-data3 <- data2[data2$Date >= as.Date("2007-02-01")
-               & data2$Date <= as.Date("2007-02-02"),]
-
 # convert the date and time column into a single Date-time value
-datetime  <- strptime(paste(data3$Date, data3$Time, sep=" "),
-                      format="%Y-%m-%d %h:%m:%s")
+datetime  <- strptime(paste(data2$Date, data2$Time, sep=" "),
+                      format="%d/%m/%Y %H:%M:%S")
 
 # create the plot
-#png("plot1.png", width=480, height=480)
-plot(datetime, data3$Global_active_power,
-     xlab="", ylab="Global Active Power (kilowatts)")
-#TODO:  set the weekdays on the X axis#
+png("plot2.png", width=480, height=480)
+plot(datetime, data2$Global_active_power,
+     xlab="", ylab="Global Active Power (kilowatts)", type="l")
 dev.off()
