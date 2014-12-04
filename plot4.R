ab3d0f72a21dbff7f54b5de200bd5ba17b2b765c
davekirby <dave.x.kirby@gmail.com>
2014-12-04 12:38:44 +0000

    updated comments

--- plot4.R
+++ plot4.R
@@ -1,14 +1,15 @@
 # Create the fourth plot for the course project
 
 # Read the data file - pipe it through grep to get just the rows we want
-# also need to get the first line for the headers
+# - we also need to get the first line for the headers.
+# keep Date and Time as strings so we can convert them afterwards.
 datafile  <- pipe("egrep '^Date|^[12]/2/2007' ./household_power_consumption.txt")
 data = read.csv2(datafile, na.strings="?", dec=".", as.is=c("Date", "Time"))
 
 # convert the date and time column into a single Date-time value
 datetime  <- strptime(paste(data$Date, data2$Time, sep=" "),
                       format="%d/%m/%Y %H:%M:%S")
-# create the plot
+# create the plots
 png("plot4.png", width=480, height=480)
 par(mfrow=c(2, 2))
 with(data, {
